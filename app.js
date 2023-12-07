42c2de9e00a62ce6d53e14e00b48b9456529c0d9
AlmasHanif <almashanif126@gmail.com>
2023-12-07 23:57:54 +0500

    stpe1

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+// let message = "Hello world";
+// console.log(message);
+//syntax error
+// lett message = "Hello World";
+// console.log(message);
+//type error
+// let message = "Hello World";
+// console.loger(message);
+//assignability error
+// let message = "Hello World";
+// message = 6;
+// console.log(message);
